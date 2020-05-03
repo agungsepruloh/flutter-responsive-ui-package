2d913ce35f29f6d2ee1dcc176cf24c15c743e7f3
Agung Sepruloh <agungsepruloh1996@gmail.com>
2020-05-03 09:41:06 +0700

    Create size config object
    Size config object is used to manage the size of each component

--- lib/responsive_ui.dart
+++ lib/responsive_ui.dart
@@ -1,7 +1,42 @@
 library responsive_ui;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'package:flutter/material.dart';
+
+class SizeConfig {
+  static double _screenWidth;
+  static double _screenHeight;
+  static double _blockWidth;
+  static double _blockHeight;
+  static bool _isPortrait;
+  static bool _isMobilePortrait;
+
+  static double get textMultiplier => _blockHeight;
+  static double get imageSizeMultiplier => _blockWidth;
+  static double get widthMultiplier => _blockWidth;
+  static double get heightMultiplier => _blockHeight;
+  static bool get isPortrait => _isPortrait;
+  static bool get isMobilePortrait => _isMobilePortrait;
+
+  void init(BoxConstraints constraints, Orientation orientation) {
+    if (orientation == Orientation.portrait)
+      setupPortraitOrientation(constraints);
+    else if (orientation == Orientation.landscape)
+      setupLandscapeOrientation(constraints);
+
+    _blockWidth = _screenWidth / 100;
+    _blockHeight = _screenHeight / 100;
+    _isMobilePortrait = _screenWidth < 650 ? true : false;
+  }
+
+  void setupPortraitOrientation(BoxConstraints constraints) {
+    _screenWidth = constraints.maxWidth;
+    _screenHeight = constraints.maxHeight;
+    _isPortrait = true;
+  }
+
+  void setupLandscapeOrientation(BoxConstraints constraints) {
+    _screenWidth = constraints.maxHeight;
+    _screenHeight = constraints.maxWidth;
+    _isPortrait = false;
+  }
 }
